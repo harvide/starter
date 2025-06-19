b87ab35666ddb7b6c015c3e093af981040f12b36
style77 <stylek777@gmail.com>
2025-06-20 00:17:07 +0200

    feat(eslint): extend ESLint configuration with repository settings

--- apps/client/eslint.config.mjs
+++ apps/client/eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import repoConfig from "@repo/config/eslint.config.mjs";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...repoConfig
 ];
 
 export default eslintConfig;

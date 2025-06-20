15c50c7c8e974149907eacda2c3508de3fd1d434
style77 <stylek777@gmail.com>
2025-06-20 20:07:18 +0200

    feat(auth): enhance authentication configuration with phone and email OTP settings

--- packages/config/src/config.ts
+++ packages/config/src/config.ts
@@ -1,7 +1,20 @@
-import type { BetterAuthPlugin } from "better-auth";
-
 const config = {
   auth: {
+    // phone number authentication
+    phone: {
+      // enabling phone number authentication also enables phone number signup 
+      enabled: true,
+
+      otp: {
+        enabled: false,
+        allowedAttempts: 5, // number of allowed attempts for OTP verification
+        otpLength: 6,
+        expiresIn: 300, // 5 minutes
+        requireVerification: true, // require phone number verification
+      }
+    },
+
+    // email+password
     emailAndPassword: {
       enabled: true,
       autoSignIn: false,
@@ -10,6 +23,21 @@ const config = {
       maxPasswordLength: 128,
       requireEmailVerification: true,
       resetPasswordTokenExpiresIn: 3600,
+
+      verificationTokenExpiresIn: 3600, // 1 hour
+      sendResetPassword: true, // allow sending reset password emails
+
+      // otp is using emailOTP plugin
+      otp: {
+        enabled: true,
+
+        otpLength: 6,
+        expiresIn: 300, // 5 minutes
+        sendVerificationOnSignUp: true,
+        disableSignUp: false,
+
+        allowedAttempts: 5,
+      }
     },
     socialProviders: {
       apple: {
@@ -29,7 +57,20 @@ const config = {
       altText: "Harvide Logo",
     },
   },
-  plugins: [] as BetterAuthPlugin[],
+  preferences: {
+    showToasts: {
+      // Show success toasts for actions like correct login, sign up, etc.
+      success: true,
+      // Show toasts for actions like incorrect login, sign up errors, etc.
+      error: true,
+      // Show toasts for informative messages like password reset, email verification, etc.
+      info: true,
+      // Show toasts for warnings like weak passwords, etc.
+      warning: true,
+
+      debug: true, // only in development
+    }
+  }
 };
 
 export default config;

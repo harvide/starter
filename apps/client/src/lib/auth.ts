e4049d1635a4a66cc770dd437ecbbdcefadb0fe9
style77 <stylek777@gmail.com>
2025-06-20 18:45:07 +0200

    feat(auth): add exports field to package.json for module resolution

--- apps/client/src/lib/auth.ts
+++ apps/client/src/lib/auth.ts
@@ -1,8 +1,14 @@
 import { config } from "@repo/config"
+import { emailOTPClient, phoneNumberClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react"
- 
-export const authClient =  createAuthClient({
+
+
+export const authClient = createAuthClient({
     baseURL: config.urls.client,
+    plugins: [
+        emailOTPClient(),
+        phoneNumberClient()
+    ],
 })
 
 export const {

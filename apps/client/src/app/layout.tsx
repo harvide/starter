7be5c3885234b1eee47b67495b404d23bfb89b47
style77 <stylek777@gmail.com>
2025-06-22 00:01:57 +0200

    feat(layout): add AdminCheckWrapper and Toaster components to RootLayout

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@repo/ui/globals.css";
+import { AdminCheckWrapper } from "@/components/wrapper/admin-check-wrapper";
+import { Toaster } from "@repo/ui/components/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <AdminCheckWrapper />
+        <Toaster />
         {children}
       </body>
     </html>

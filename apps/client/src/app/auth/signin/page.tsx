1672a30dbec2df87e94be4c688af01010e524561
style77 <stylek777@gmail.com>
2025-06-21 13:12:18 +0200

    refactor(auth): update SignInPage to use dynamic login form variant based on config

--- apps/client/src/app/auth/signin/page.tsx
+++ apps/client/src/app/auth/signin/page.tsx
@@ -1,11 +1,14 @@
-import { LoginForm } from "@/components/auth/login-form"
+"use client";
 
-export default async function SigninPage() {
-    return (
-        <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-            <div className="w-full max-w-sm md:max-w-3xl">
-                <LoginForm />
-            </div>
-        </div>
-    )
+import { getLoginFormVariant } from "@/components/auth";
+import { config } from "@repo/config";
+
+export default function SignInPage() {
+  const LoginForm = getLoginFormVariant(config.ui.loginForm);
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center bg-background p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
+      </div>
+    </div>);
 }

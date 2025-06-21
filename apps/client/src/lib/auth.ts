93f7ebfd3cff6ea170bf7402c94c1353ddf92405
style77 <stylek777@gmail.com>
2025-06-21 22:07:08 +0200

    chore(admin): remove admin app files and configuration

--- apps/client/src/lib/auth.ts
+++ apps/client/src/lib/auth.ts
@@ -1,10 +1,7 @@
-import { config } from "@repo/config"
 import { emailOTPClient, phoneNumberClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react"
 
-
 export const authClient = createAuthClient({
-    baseURL: config.urls.client,
     plugins: [
         emailOTPClient(),
         phoneNumberClient()

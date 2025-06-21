a8cf153b628af0aed392c5e2f9ada450f772995c
style77 <stylek777@gmail.com>
2025-06-21 13:43:07 +0200

    feat(auth): add metadata for SignInPage and handle missing login form variant

--- apps/client/src/app/auth/signin/page.tsx
+++ apps/client/src/app/auth/signin/page.tsx
@@ -1,10 +1,18 @@
-"use client";
-
 import { getLoginFormVariant } from "@/components/auth";
 import { config } from "@repo/config";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: `Sign in - ${config.branding.name}`,
+  description: config.branding.description,
+};
 
 export default function SignInPage() {
   const LoginForm = getLoginFormVariant(config.ui.loginForm);
+  if (!LoginForm) {
+    throw new Error(`Login form variant "${config.ui.loginForm}" not found.`);
+  }
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-background p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">

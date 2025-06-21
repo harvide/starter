9c86ce37ae55d9a936ead22e9cb1c6ed82ba8139
style77 <stylek777@gmail.com>
2025-06-22 00:32:22 +0200

    feat(auth): add header and subtitle to LoginForm in AdminSignInPage

--- apps/client/src/app/admin/signin/page.tsx
+++ apps/client/src/app/admin/signin/page.tsx
@@ -52,7 +52,16 @@ async function AdminSignInPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-background p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <LoginForm forceEmailAndPasswordOnly />
+        <LoginForm forceEmailAndPasswordOnly header={
+          <>
+            Admin Sign In
+          </>
+        }
+          subtitle={
+            <>
+              Please sign in to access the admin dashboard.
+            </>
+          } />
       </div>
     </div>
   );

03d10cb61bd8a26a87a75ba13bcfd7065334ef24
style77 <stylek777@gmail.com>
2025-06-22 13:10:04 +0200

    feat(utils): add getAcronym function to generate acronyms from names

---
+++ apps/client/src/lib/utils.ts
@@ -0,0 +1,9 @@
+export const getAcronym = (name: string) => {
+    const words = name.split(" ")
+    if (words.length === 1) {
+        return words[0]?.charAt(0).toUpperCase() || ""
+    }
+    return words
+        .map((word) => word.charAt(0).toUpperCase())
+        .join("")
+}

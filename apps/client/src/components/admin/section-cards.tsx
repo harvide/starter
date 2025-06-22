5ec6f9d6477e8e235c9e1340f244938eabee5b3e
style77 <stylek777@gmail.com>
2025-06-22 19:16:52 +0200

    feat(admin): enhance SectionCards component to handle metric card data more robustly

--- apps/client/src/components/admin/section-cards.tsx
+++ apps/client/src/components/admin/section-cards.tsx
@@ -1,19 +1,20 @@
 import { config } from "@repo/config"
-import { MetricCard, resolveMetricCard } from "@/components/admin/dashboard/metric-card"
+import { MetricCard, MetricCardSkeleton, resolveMetricCard } from "@/components/admin/dashboard/metric-card"
 
-export function SectionCards() {
+export async function SectionCards() {
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
-      {config.admin.dashboard.metrics.map(async (metric) => {
-        const metricData = await resolveMetricCard(metric.type);
+      {config.admin.dashboard.metrics.map(async (metric, i) => {
+        const cardData = await resolveMetricCard(metric.type);
+        if (!cardData) return null;
         return (
           <MetricCard
-            key={metricData.id}
-            id={metricData.id}
-            label={metricData.label}
-            value={metricData.value}
-            badge={metricData.badge}
-            footer={metricData.footer}
+            key={cardData.id}
+            id={cardData.id}
+            label={cardData.label}
+            value={cardData.value}
+            badge={cardData.badge}
+            footer={cardData.footer}
           />
         )
       })}

55f9ba1b00e52001f0ebcf50fa37c3778b7927a5
style77 <stylek777@gmail.com>
2025-06-22 11:52:43 +0200

    feat(admin): replace link with button in admin account warning message for improved UI

--- apps/client/src/components/wrapper/admin-check-wrapper.tsx
+++ apps/client/src/components/wrapper/admin-check-wrapper.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { toast } from "sonner";
 import { checkAdminsExist } from "@/components/auth/admin-signup-form/flows";
 import Link from "next/link";
+import { Button } from "@repo/ui/components/button";
 
 export function AdminCheckWrapper() {
     useEffect(() => {
@@ -11,10 +12,14 @@ export function AdminCheckWrapper() {
             const res = await checkAdminsExist();
             if (res?.error || res.exists) return;
 
+            if (window.location.pathname === "/admin/signin") {
+                return;
+            }
+
             setTimeout(() => {
                 toast.warning(<>No Admin Account Found</>, {
                     description: (
-                        <>Please create the first admin account at <Link href="/admin/signin">/admin/signin</Link></>
+                        <>Please create the first admin account at <Button asChild variant="link" size="sm" className="p-0 m-0 gap-0 h-auto font-normal"><Link href="/admin/signin">/admin/signin</Link></Button></>
                     )
                 });
             }, 100);

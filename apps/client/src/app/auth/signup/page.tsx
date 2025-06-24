6d05da9a7d9f88f00bf8ec8c33c2b5905682a962
style77 <stylek777@gmail.com>
2025-06-24 21:03:04 +0200

    feat(auth): display error message and redirect to sign in when signup is disabled

--- apps/client/src/app/auth/signup/page.tsx
+++ apps/client/src/app/auth/signup/page.tsx
@@ -2,6 +2,9 @@ import { Metadata } from "next";
 import { config } from "@repo/config";
 
 import { getSignupFormVariant } from "@/components/auth/signup-form";
+import Link from "next/link";
+import { Button } from "@repo/ui/components/button";
+import { ToastOnPageLoadWrapper } from "@/components/wrapper/toast-client-wrapper";
 
 export const metadata: Metadata = {
     title: `Sign up - ${config.branding.name}`,
@@ -12,6 +15,27 @@ export default function SignupPage() {
     const BasicSignupForm = getSignupFormVariant(config.ui.signupForm);
     if (!BasicSignupForm) {
         throw new Error(`Signup form variant "${config.ui.signupForm}" not found.`);
+    }
+
+    if (config.auth.emailAndPassword.disableSignUp) {
+        return (
+            <>
+                <ToastOnPageLoadWrapper type="error" message={<>Signup is currently disabled.</>} data={{description: <>Please contact support for assistance.</>}} />
+                <div className="flex min-h-svh flex-col items-center justify-center bg-background p-6 md:p-10">
+                    <div className="w-full max-w-sm md:max-w-3xl">
+                        <h1 className="text-2xl font-bold text-center">Signup Disabled</h1>
+                        <p className="text-center mt-4">Signup is currently disabled. Please contact support for assistance.</p>
+                    </div>
+                    <div className="mt-6 flex flex-row items-center self-center">
+                        <Link href="/auth/signin">
+                            <Button variant="default" className="w-full">
+                                Back to Sign In
+                            </Button>
+                        </Link>
+                    </div>
+                </div>
+            </>
+        );
     }
 
     return (

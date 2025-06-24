02a5df0c7ccc03a01f263229a33490cc5da4d28b
style77 <stylek777@gmail.com>
2025-06-24 21:03:00 +0200

    feat(ui): add ToastOnPageLoadWrapper component to display toast messages on page load

---
+++ apps/client/src/components/wrapper/toast-client-wrapper.tsx
@@ -0,0 +1,34 @@
+"use client";
+// Show toast on page load
+import { useEffect } from "react";
+import { showToast } from "@/lib/toast";
+import { ExternalToast } from "sonner";
+
+export type ToastOnPageLoadWrapperProps = {
+    type: "info" | "success" | "warning" | "error";
+    message: string | React.ReactNode;
+    data?: ExternalToast;
+}
+
+export function ToastOnPageLoadWrapper({ type, message, data }: ToastOnPageLoadWrapperProps) {
+    useEffect(() => {
+        setTimeout(() => {
+            switch (type) {
+                case "info":
+                    showToast.info(message, data);
+                    break;
+                case "success":
+                    showToast.success(message, data);
+                    break;
+                case "warning":
+                    showToast.warning(message, data);
+                    break;
+                case "error":
+                    showToast.error(message, data);
+                    break;
+            }
+        }, 100);
+    }, []);
+
+    return <></>;
+}
